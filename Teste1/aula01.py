751ef9aa6093dc17103c176d82c3058461fdba28
Your Name <you@example.com>
2025-11-17 20:43:29 -0300

    ok

---
+++ Teste1/aula01.py
@@ -0,0 +1,51 @@
+numero = 10
+numero = 20
+
+print(numero+numero)
+ 
+"""
+x = 1
+while x <= 10:
+    print (x)
+    x = x + 1
+"""
+"""
+for i in range (5):
+    print(i)
+"""
+"""
+x = 1
+while True:
+    print(x)
+"""
+"""
+n = int (input("Digite um número: "))
+x = 1
+while x <= 10:
+    print(n+x)
+    x = x+1 #contador
+"""
+"""
+x = 1
+soma = 0
+while x <= 10:
+    n = int (input("Digite um número: "))
+    soma = soma + n #acumulador
+    x = x+1
+print (soma)
+"""
+"""
+x = 1
+while x <= 10:
+    if x == 5:
+        break
+print (x)
+x = x + 1
+"""
+x = 0
+while True:
+    num = int(input ("Digite um numero: "))
+    if num == 0:
+        break
+    x = x+num
+print (x)
